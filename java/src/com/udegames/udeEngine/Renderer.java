1ad04963c0fd5bd5eef8977c8be452bf7f04aba6
RafD <43214870+RafDiol@users.noreply.github.com>
2020-08-04 21:32:19 +0300

    Renderer
    Added some extra functionallity

--- java/src/com/udegames/udeEngine/Renderer.java
+++ java/src/com/udegames/udeEngine/Renderer.java
@@ -1,7 +1,9 @@
 package com.udegames.udeEngine;
 
 import com.udegames.udeEngine.componetSystem.Object;
+import sun.awt.image.ToolkitImage;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
@@ -9,6 +11,7 @@ public class Renderer {
 
     private Engine engine;
     private int[] p;
+    private boolean isDrawn[][] = new boolean[4000][4000];
 
     public Renderer(Engine engine) {
         this.engine = engine;
@@ -24,7 +27,16 @@ public class Renderer {
     public void drawImage(BufferedImage image, int scaleX, int scaleY, int x, int y) {
         int height = image.getHeight(), width = image.getWidth();
         engine.getWindow().getImage().getGraphics().drawImage(image, x, y, scaleX * width, scaleY * height, null);
+        isDrawn[x][y] = true;
     }
+
+    //This part by RafD
+    public void drawImage(BufferedImage image, int x, int y){
+        int scaleX = 50, scaleY = 50;
+        drawImage(image, scaleX, scaleY, x, y);
+        isDrawn[x][y] = true;
+    }
+
 
     public void drawObject(Object object, int scaleX, int scaleY) {
         drawImage(object.getImage(), scaleX, scaleY, object.getX(), object.getY());
@@ -32,14 +44,21 @@ public class Renderer {
 
     public void drawObject(Object object, int scaleX, int scaleY, int x, int y) {
         drawImage(object.getImage(), scaleX, scaleY, x, y);
+        isDrawn[x][y] = true;
     }
 
     public void drawString(String text, int x, int y) {
         engine.getWindow().getImage().getGraphics().drawString(text, x, y);
+        isDrawn[x][y] = true;
     }
 
     // This part by RafD
     public void drawLine(int x1, int y1, int x2, int y2) {
         engine.getWindow().getImage().getGraphics().drawLine(x1, y1, x2, y2);
+    }
+
+    // This part by RafD
+    public boolean isAvailable(int x, int y) throws ArrayIndexOutOfBoundsException{
+        return isDrawn[x][y];
     }
 }
